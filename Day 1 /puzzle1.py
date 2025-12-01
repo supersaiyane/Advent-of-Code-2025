a825cef784cf04f49f09bfbc45bd890ff3c155c3
Gurpreet Singh <49033319+supersaiyane@users.noreply.github.com>
2025-12-01 12:53:51 +0000

    Update puzzle1.py

--- Day 1 /puzzle1.py
+++ Day 1 /puzzle1.py
@@ -1,1 +1,21 @@
+"""
+count how many times the dial ENDS a rotation pointing at 0
+"""
 
+def solve_part1(lines):
+  pos = 50
+  count = 0
+
+  for line in lines:
+    d = line[0]
+    n = int(line[1:])
+
+    if d == 'L'
+      pos =  (pos - n) % 100
+    else:
+      pos = (pos - n) % 100
+
+    if pos == 0:
+      count += 1
+  return count    
+      
