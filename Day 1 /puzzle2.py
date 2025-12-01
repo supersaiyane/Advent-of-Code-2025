f5a1851a99699365a387761a14bcd3beae7cba46
Gurpreet Singh <49033319+supersaiyane@users.noreply.github.com>
2025-12-01 12:58:56 +0000

    Update puzzle2.py

--- Day 1 /puzzle2.py
+++ Day 1 /puzzle2.py
@@ -1,1 +1,23 @@
+"""
+count how many times ANY CLICK cause the dial to point at 0.
+"""
 
+pos = 50
+count = 0
+
+for line in lines:
+  d = line[0]
+  n = int(line[1:])
+
+  if d == 'R'
+    count += (pos+n) // 100 - (pos // 100)
+    pos = (pos +n ) % 100
+else:
+  if pos == 0:
+    count += n//100
+  else:
+    if n >= pos:
+      count += (n-pos) // 100+1
+  pos = (pos -n) % 100
+
+ return count
